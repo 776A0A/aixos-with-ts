b220f57450551285ea0c85b948bcea44fd18262c
har3dy <har3dy@icloud.com>
2020-09-14 16:55:53 +0800

    feat(src/xhr): 添加错误处理
    处理网络错误、超时错误、status不合法错误

--- src/xhr.ts
+++ src/xhr.ts
@@ -3,15 +3,17 @@ import { parseHeaders } from './helpers/header'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve, reject) => {
-    const { data = null, url, method = 'get', headers, responseType } = config
+    const { data = null, url, method = 'get', headers, responseType, timeout = 0 } = config
     const request = new XMLHttpRequest()
 
     responseType && (request.responseType = responseType)
+    timeout && (request.timeout = timeout)
 
     request.open(method.toLocaleUpperCase(), url, true)
 
     request.onreadystatechange = function handleLoad() {
       if (request.readyState !== 4) return
+      if (request.status === 0) return
 
       const responseHeaders = parseHeaders(request.getAllResponseHeaders()) // 拿到的结果是一个字符串，所以需要转换为对象
       const responseData = responseType !== 'text' ? request.response : request.responseText
@@ -25,12 +27,25 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         request
       }
 
-      resolve(response)
+      handleResponse(response)
     }
+
+    request.onerror = function handleError() {
+      reject(new Error('Network Error'))
+    }
+    request.ontimeout = function handleTimeout() {
+      reject(new Error(`Timeout of ${timeout} ms exceeded`))
+    }
+
     data &&
       Object.entries(headers).forEach(([name, val]) => {
         request.setRequestHeader(name, val as string)
       })
     request.send(data)
+
+    function handleResponse(response: AxiosResponse): void {
+      if (response.status >= 200 && response.status < 300) resolve(response)
+      else reject(new Error(`Request failed width status code ${response.status}`))
+    }
   })
 }
